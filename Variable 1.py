19605d0dc87cc431b730aec0169bdfee39595db8
Андрей <afnnebo@gmail.com>
2024-08-07 21:08:01 +0300

    Delete Variable 1.py

--- Variable 1.py
+++
@@ -1,7 +0,0 @@
-DZ = 'The number of completed:'
-a = 12
-name = "Number of hours spent:"
-b = 1.5
-Course_name = 'Python,'
-Time = 'Time_for_one_task'
-print( Course_name, DZ,  a, name, b,  Time, b/a)
