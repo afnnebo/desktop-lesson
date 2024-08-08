79f05a4161235d0f3c05f114da8525c8eb98e864
Андрей <afnnebo@gmail.com>
2024-08-08 18:19:58 +0300

    Add files via upload

---
+++ Module_1_5.py
@@ -0,0 +1,19 @@
+immutable_var=10,12,14,16,18,True,'string'
+immutable_var1=(11,13,15,17)
+immutable_var2=tuple([7,8,9])
+print(type(immutable_var))
+print(immutable_var)
+print(immutable_var1)
+print(immutable_var2)
+# immuble_varta[1]=22 # нельзя менять элементы кортежа - приводит к ошибке
+mutable_list=["10","12","14","16","18","True","string" ]
+print(mutable_list[5])
+mutable_list[5]='fols'
+print(mutable_list)
+
+
+
+
+
+
+
