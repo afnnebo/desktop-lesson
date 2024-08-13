08756712bcfe49abf2ccee71475d95fe38e25592
Андрей <afnnebo@gmail.com>
2024-08-13 19:09:35 +0300

    Add files via upload

--- Module_1_4.py
+++ Module_1_4.py
@@ -1,7 +1,8 @@
 my_string='добро пожаловать в урбан'
 len(my_string)
+print(len(my_string))
 print(my_string.upper())
 print(my_string.lower())
-print(my_string.replace(' ', '#'))
+print(my_string.replace(' ', ''))
 print(my_string [0])
 print(my_string [23])
