d05059c41761bf24eded37c1e0f15e853e4451f9
Андрей <afnnebo@gmail.com>
2024-08-14 14:49:27 +0300

    Add files via upload

---
+++ Module_1_6.py
@@ -0,0 +1,22 @@
+my_dict={'Andrei': 1962, 'Tatyna': 1959, 'Vasiliy': 1987, 'Aleksandr': 2003}
+print(my_dict)# Выведите на экран словарь my_dict.
+print(my_dict['Andrei'])# Выведите на экран одно значение по существующему ключу
+my_dict['grigorii']= 2002
+print(my_dict) # Выведите на экран словарь my_dict.
+print(my_dict['grigorii']) # Выведите на экран одно по отсутствующему из словаря my_dict
+my_dict[('Anatolii')]= 1967 # Добавьте ещё две произвольные пары того же формата в словарь my_dict.
+my_dict['Irina']= 1964
+print(my_dict) # # Выведите на экран словарь my_dict.
+del my_dict ['Irina'] # Удалите одну из пар в словаре по существующему ключу
+print(my_dict.get('Irina')) # выведите значение из этой пары на экран.
+print(my_dict)  # Выведите на экран словарь my_dict.
+#
+#
+my_set={1,2,3,4,5,1,2,3,'slovo',(3,14)} # Создайте переменную и присвойте ей множество, состоящее из разных типов данных с повторяющимися значениями
+print(my_set)# Выведите на экран множество (должны отобразиться только уникальные значения)
+print(my_set.add('mnogestvo'))
+print(my_set.add(10))
+print(my_set)
+print(my_set.discard('slovo'))
+print(my_set)
+
