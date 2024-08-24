b70d4647babaf8e36213cc6e0905f361a9b7d22b
Андрей <afnnebo@gmail.com>
2024-08-24 17:12:27 +0300

    Add files via upload

---
+++ Module_2_3.py
@@ -0,0 +1,16 @@
+while True:
+    my_list = [42, 69, 322, 13, 0, 99, -5, 9, 8, 7, -6, 5]
+    number = [0]
+    if  number != len(my_list):
+        print(my_list[0])==len(my_list)
+        print(my_list[1])==len(my_list)
+        print(my_list[2])==len(my_list)
+        print(my_list[3])==len(my_list)
+        print(my_list[5])==len(my_list)
+        break
+    else:
+        print(number)
+        continue
+print ("number", [0],  "не отрицательное и не положительное число")
+
+
