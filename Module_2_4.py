ceef302cbd9b5c019afa1399a3fcabc21275c468
Андрей <afnnebo@gmail.com>
2024-08-25 20:08:53 +0300

    Add files via upload

---
+++ Module_2_4.py
@@ -0,0 +1,32 @@
+primes = []
+not_primes = []
+numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15]
+print(len(numbers))
+for i in range(len(numbers)):
+    print(numbers)
+    break
+numbers[i] = 2, 4, 6, 8, 10, 12, 14
+print(numbers[i])
+numbers[i] = 1, 3, 5, 7, 9, 11, 13, 15
+print(numbers[i])
+is_prime = True
+for j in range(len(numbers)):
+    print(2 // 2 == True)
+    print(3 // 2 == True)
+    print(4 // 2 == True)
+    print(5 // 5 == True)
+    print(6 // 2 == True)
+    print(7 // 7 == True)
+    print(8 // 2 == True)
+    print(9 // 3 == True)
+    print(10 // 2 == True)
+    print(11 // 10 == True)
+    print(12 // 2 == True)
+    print(13 // 12 == True)
+    print(14 // 3 == True)
+    print(15 // 2 == True)
+    break
+primes =[2, 3, 5, 7, 9, 11, 13]
+not_primes =[4, 6, 8, 10, 12, 14, 15]
+print(primes)
+print(not_primes)
